eda0e362314ff6144012b3a92a389f57949031ea
SkyOfHope <1452524591@qq.com>
2018-10-16 14:29:24 +0800

    价目册首次提交

---
+++ JYPriceList/Interface/GlassShopViewController(镜片商城)/View/ShowQrcodeView.m
@@ -0,0 +1,101 @@
+//
+//  ShowQrcodeView.m
+//  JYPriceList
+//
+//  Created by jiangkuiquan on 2018/9/11.
+//  Copyright © 2018年 jiangkuiquan. All rights reserved.
+//
+
+#import "ShowQrcodeView.h"
+
+
+@interface ShowQrcodeView ()
+
+@end
+
+@implementation ShowQrcodeView
+
+-(instancetype)init{
+    if (self) {
+        self = [[NSBundle mainBundle] loadNibNamed:@"ShowQrcodeView" owner:self options:nil].lastObject;
+        
+//        UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressSave)];
+//        longPress.minimumPressDuration = 2;
+        UITapGestureRecognizer *longPress = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(longPressSave)];
+        
+        [self.qrcodeImg addGestureRecognizer:longPress];
+        
+    }
+    
+    return self;
+}
+
+//隐藏界面
+-(void)hideControl{
+    [UIView animateWithDuration:.3 animations:^{
+        
+    }completion:^(BOOL finished) {
+        [self removeFromSuperview];
+    }];
+    
+}
+
+#pragma mark - 点击事件
+
+-(void)longPressSave{
+    NSLog(@"长安点击下载图片");
+    
+    dispatch_queue_t globalQueue = dispatch_get_global_queue(0, 0);
+    dispatch_async(globalQueue, ^{
+        
+        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:self.imgUrlStr]];
+        UIImage *image = [UIImage imageWithData:data]; // 取得图片
+        [self saveImageToPhotos:image];
+//        [s];
+    });
+    
+
+}
+
+
+//将图片保存到相册
+- (void)saveImageToPhotos:(UIImage*)savedImage{
+    UIImageWriteToSavedPhotosAlbum(savedImage, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+}
+
+- (void)image: (UIImage *) image didFinishSavingWithError: (NSError *) error contextInfo: (void *) contextInfo{
+    
+    NSString *msg = nil ;
+    if(error != NULL){
+        msg = @"保存图片失败" ;
+    }else{
+        msg = @"保存图片成功" ;
+    }
+
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"保存图片结果提示"
+                                                    message:msg
+                                                   delegate:self
+                                          cancelButtonTitle:@"确定"
+                                          otherButtonTitles:nil];
+    [alert show];
+    
+    [self hideControl];
+}
+
+- (IBAction)buttonClickAction:(UIButton *)sender {
+    [self hideControl];
+}
+
+
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
